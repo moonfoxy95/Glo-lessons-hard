9f19ebf4eee738dc87faa4eb57fc5bc0d93c41f3
vlad laptop <moonfoxy@icloud.com>
2026-03-17 20:14:28 +0300

    done lesson 4

--- script.js
+++ script.js
@@ -1,47 +1,17 @@
 'use strict';
 
-// Задание 1
-let lang = 'ru';
-
-let weekdaysRu = 'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье';
-let weekdaysEn = 'monday, tuesday, wensday, thursday, friday, saturday, sunday';
-let weekdaysErr = 'Язык не ru или en'
-
-if (lang === 'ru') {
-  console.log(weekdaysRu);
-} else if (lang === 'en') {
-  console.log(weekdaysEn);
-} else {
-  console.log(weekdaysErr);
+function lesson4(oneArgument) {
+  if (typeof oneArgument !== 'string') {
+    return 'Аргумент не строка.';
+  } else {
+    let modifiedOneArgument = oneArgument.trim();
+    if (modifiedOneArgument.length > 30) {
+      modifiedOneArgument = modifiedOneArgument.substring(0, 30) + '...';
+    }
+    return modifiedOneArgument;
+  }
 }
 
-switch (lang) {
-  case 'ru':
-    console.log(weekdaysRu);
-    break;
-  case 'en':
-    console.log(weekdaysEn);
-    break;
-  default:
-    console.log(weekdaysErr);
-}
+let myArgument = 'Oberfeldwaldwiesenundhaushofmeister';
 
-let weeksArray = {
-  'ru': [weekdaysRu],
-  'en': [weekdaysEn]
-}
-
-if (weeksArray[lang]) {
-  console.log(weeksArray[lang].join(', '));
-} else {
-  console.log(weekdaysErr);
-}
-
-// Задание 2
-let namePerson = 'Артем';
-
-namePerson === 'Артем'
-  ? console.log('директор')
-  : namePerson === 'Александр'
-    ? console.log('преподаватель')
-    : console.log('студент')
+console.log(lesson4(myArgument));
