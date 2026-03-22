d979e7bcdf3124e558bd74b8618b769eb952303b
vlad laptop <moonfoxy@icloud.com>
2026-03-22 13:44:27 +0300

    done lesson 5

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 'use strict';
 
-function lesson4(oneArgument) {
-  if (typeof oneArgument !== 'string') {
-    return 'Аргумент не строка.';
-  } else {
-    let modifiedOneArgument = oneArgument.trim();
-    if (modifiedOneArgument.length > 30) {
-      modifiedOneArgument = modifiedOneArgument.substring(0, 30) + '...';
-    }
-    return modifiedOneArgument;
+// Усложненное 1 (строка 15)
+
+let screenPrice;
+
+let isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+do {
+  screenPrice = '   500   ' || prompt('Сколько будет стоить данная работа?')
+} while (!isNumber(screenPrice));
+
+screenPrice = +screenPrice;
+
+console.log(screenPrice, typeof (screenPrice));
+
+// Усложненное 2
+
+let myArr = ['102', '20', '30', '40', '50', '60', '70'];
+
+for (let i = 0; i < myArr.length; i++) {
+  if (myArr[i][0] === '2' || myArr[i][0] === '4') {
+    console.log(myArr[i]);
   }
 }
 
-let myArgument = 'Oberfeldwaldwiesenundhaushofmeister';
 
-console.log(lesson4(myArgument));
+let simpleDigit = function () {
+  for (let i = 2; i <= 100; i++) {
+
+    let isSimple = true;
+
+    for (let j = 2; j < Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        isSimple = false;
+        break;
+      }
+    }
+
+    if (isSimple) {
+      console.log(i);
+    }
+  }
+}
+
+simpleDigit();
